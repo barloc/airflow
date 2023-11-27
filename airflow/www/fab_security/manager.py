1e545a6db5f1c79c59c777b45e998041dcf07615
Stanislav Lysikov <s.lysikov@space307.com>
2023-11-27 15:58:39 +0300

    feature: add ldap nested groups for role binding

--- airflow/www/fab_security/manager.py
+++ airflow/www/fab_security/manager.py
@@ -249,6 +249,8 @@ class BaseSecurityManager:
             app.config.setdefault("AUTH_LDAP_FIRSTNAME_FIELD", "givenName")
             app.config.setdefault("AUTH_LDAP_LASTNAME_FIELD", "sn")
             app.config.setdefault("AUTH_LDAP_EMAIL_FIELD", "mail")
+            # Nested groups options
+            app.config.setdefault("AUTH_LDAP_USE_NESTED_GROUPS_FOR_ROLES", False)
 
         if self.auth_type == AUTH_OID:
             from flask_openid import OpenID
@@ -501,6 +503,10 @@ class BaseSecurityManager:
     def auth_ldap_tls_keyfile(self):
         """LDAP TLS key file."""
         return self.appbuilder.get_app.config["AUTH_LDAP_TLS_KEYFILE"]
+
+    @property
+    def auth_ldap_use_nested_groups_for_roles(self):
+        return self.appbuilder.get_app.config["AUTH_LDAP_USE_NESTED_GROUPS_FOR_ROLES"]
 
     @property
     def openid_providers(self):
@@ -956,10 +962,36 @@ class BaseSecurityManager:
             user_dn = search_result[0][0]
             # extract the other attributes
             user_info = search_result[0][1]
-            # return
-            return user_dn, user_info
         except (IndexError, NameError):
             return None, None
+
+        # get nested groups for user
+        if self.auth_ldap_use_nested_groups_for_roles:
+            log.debug("Nested groups for LDAP enabled.")
+
+            # filter for microsoft active directory only
+            nested_groups_filter_str = (
+                f"(&(objectCategory=Group)(member:1.2.840.113556.1.4.1941:={user_dn}))"
+            )
+            nested_groups_request_fields = ["cn"]
+
+            nested_groups_search_result = con.search_s(
+                self.auth_ldap_search,
+                ldap.SCOPE_SUBTREE,
+                nested_groups_filter_str,
+                nested_groups_request_fields,
+            )
+            log.debug("LDAP search returned: %s", nested_groups_search_result)
+
+            nested_groups = [
+                x[0] for x in nested_groups_search_result if x[0] is not None
+            ]
+            log.debug("LDAP nested groups for users: %s", nested_groups)
+
+            user_info[self.auth_ldap_group_field] = nested_groups
+
+        # return
+        return user_dn, user_info
 
     def _ldap_calculate_user_roles(self, user_attributes: dict[str, list[bytes]]) -> list[str]:
         user_role_objects = set()
